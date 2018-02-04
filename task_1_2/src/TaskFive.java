1dd31696268d9704b71a40b2a00dffe7ed1afba8
ivm23 <marina200964@yandex.ru>
2018-02-04 18:39:36 +0300

    fixed problem 5

--- task_1_2/src/TaskFive.java
+++ task_1_2/src/TaskFive.java
@@ -1,13 +1,39 @@
+import java.util.Scanner;
+
 public class TaskFive {
 
-    public static final int N = 5;
-
     public static void main(String[] args) {
-        int[][] array = new int[N][N];
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите размер массива: ");
+        int size = in.nextInt();
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                array[i][j] = i == j || i == (N - j - 1) ? 1 : 0;
+        while (size <= 0) {
+            System.out.println("Размер массива не может быть отрицательным, введите размер массива: ");
+            size = in.nextInt();
+        }
+        int[][] array = new int[size][size];
+
+        int n, m;
+        System.out.println("Введите n:");
+        n = in.nextInt();
+
+        System.out.println("Введите m:");
+        m = in.nextInt();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                    if (i == j) {
+                        array[i][j] = n;
+                    }
+                    else {
+                        if (i == (size - j - 1)) {
+                            array[i][j] = m;
+                        }
+                }
+
+                if (size % 2 != 0) {
+                        array[size / 2][size / 2] = 0;
+                }
                 System.out.printf("%d ", array[i][j]);
             }
             System.out.println();
