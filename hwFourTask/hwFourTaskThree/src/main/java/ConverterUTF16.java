6991962674b6766684195e49878be412ae9f1a81
ivm23 <marina200964@yandex.ru>
2018-03-18 01:04:02 +0300

    fixed work with io streams

--- hwFourTask/hwFourTaskThree/src/main/java/ConverterUTF16.java
+++ hwFourTask/hwFourTaskThree/src/main/java/ConverterUTF16.java
@@ -6,16 +6,33 @@ import java.io.*;
 public class ConverterUTF16 {
 
     public static void convert(String inFileName, String outFileName) throws IOException {
-        BufferedReader inFile = new BufferedReader(new FileReader(inFileName));
+        try (BufferedReader inFile = new BufferedReader(new FileReader(inFileName))) {
 
-        Writer outFile = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFileName), "UTF16"));
+            try (Writer outFile = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFileName), "UTF16"))) {
 
-        String readedString = "";
+                String readString;
 
-        while ((readedString = inFile.readLine()) != null) {
-            outFile.write(readedString);
+                while ((readString = inFile.readLine()) != null) {
+                    System.out.println(readString);
+                    outFile.write(readString);
+                }
+            }
+            catch (FileNotFoundException ex) {
+                System.out.println("outFile not found");
+                throw new FileNotFoundException("outFile not found");
+            }
+            catch (IOException ex) {
+                System.out.println("IOException with outFile");
+                throw new IOException("IOException with outFile");
+            }
         }
-        inFile.close();
-        outFile.close();
+        catch (FileNotFoundException ex) {
+            System.out.println("inFile not found");
+            throw new FileNotFoundException("inFile not found");
+        }
+        catch (IOException ex) {
+            System.out.println("IOException with inFile");
+            throw new IOException("IOException with inFile");
+        }
     }
 }
--- hwFourTask/hwFourTaskThree/src/test/java/ConverterUTF16Test.java
+++ hwFourTask/hwFourTaskThree/src/test/java/ConverterUTF16Test.java
@@ -2,13 +2,13 @@ package test.java;
 import main.java.ConverterUTF16;
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class ConverterUTF16Test {
 
-    @Test(expected = IOException.class)
-    public void convertTestSgouldCatchIOException() throws IOException {
-        ConverterUTF16.convert("C:\\Users\\marina\\IdeaProjects\\hwTreeTaskThree\\src\\inExample","C:\\Users\\marina\\IdeaProjects\\hwTreeTaskThree\\src\\outputExample");
+    @Test(expected = FileNotFoundException.class)
+    public void convertTestShouldCatchFileNotFoundException() throws IOException {
+        ConverterUTF16.convert("src\\inExample","src\\outputExample");
     }
-
 }
